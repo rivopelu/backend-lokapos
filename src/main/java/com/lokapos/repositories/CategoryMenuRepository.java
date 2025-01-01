47df6f71ebc2e5af0190d54b3811d8ddaa631f2a
rivo pelu <nicolai.tarun@feesites.com>
2025-01-02 00:48:47 +0800

    FEAT:add migration category menu

---
+++ src/main/java/com/lokapos/repositories/CategoryMenuRepository.java
@@ -0,0 +1,7 @@
+package com.lokapos.repositories;
+
+import com.lokapos.entities.CategoryMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryMenuRepository extends JpaRepository<CategoryMenu, String> {
+}
