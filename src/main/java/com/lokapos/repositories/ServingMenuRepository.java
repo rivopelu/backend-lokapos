10ebe9c760641ff8ee231af4ee56b7cbaf88868c
rivo pelu <nicolai.tarun@feesites.com>
2025-01-02 02:32:34 +0800

    FEAT:fixing

---
+++ src/main/java/com/lokapos/repositories/ServingMenuRepository.java
@@ -0,0 +1,7 @@
+package com.lokapos.repositories;
+
+import com.lokapos.entities.ServingMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServingMenuRepository extends JpaRepository<ServingMenu, String> {
+}
