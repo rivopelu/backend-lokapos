4f58752a00dfa785b7a32dbe0ef49f1978c5f1c9
rivo pelu <dusty.emit@finestudio.org>
2025-01-10 19:04:34 +0700

    FEAT:add column total item order

--- src/main/java/com/lokapos/entities/ServingOrder.java
+++ src/main/java/com/lokapos/entities/ServingOrder.java
@@ -33,5 +33,8 @@ public class ServingOrder extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private ORDER_PAYMENT_STATUS_ENUM paymentStatus;
 
+    @Column(name = "total_item")
+    private BigInteger totalItem;
+
 
 }
---
+++ src/main/java/com/lokapos/model/response/ResponseListOrder.java
@@ -0,0 +1,27 @@
+package com.lokapos.model.response;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.lokapos.enums.ORDER_STATUS_ENUM;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonSerialize
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ResponseListOrder {
+    private String id;
+    private ORDER_STATUS_ENUM status;
+    private BigInteger totalOrder;
+    private BigInteger totalItem;
+
+}
